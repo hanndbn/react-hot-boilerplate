8de4b527ce3df07c706623461a6173760d791db4
hannd <hannd@oneempower.com>
2017-05-12 14:02:03 +0700

    dept 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
+    render() {
+        return (
+            <ExampleForm/>
+        );
+    }
 }
+
+var TextBox = React.createClass({
+    render: function () {
+        return (
+            <input
+                type="text"
+                className="form-control"
+                name={this.props.name}
+                onChange={this.props.onChange}
+                placeholder={'Please input ' + this.props.name}
+            />
+        );
+    }
+});
+
+var ExampleForm = React.createClass({
+    getInitialState: function () {
+        return ({
+            form: {firstName: 'han1', lastName: 'nguyen dinh'}
+        });
+
+    },
+    onChange: function (event) {
+        this.state.form[event.target.name] = event.target.value;
+        this.setState({form: this.state.form});
+    },
+    onSubmit: function (event) {
+        event.preventDefault();
+        alert('firstName: ' + this.state.form.firstName +
+        ' lastName: ' + this.state.form.lastName);
+    },
+    render: function () {
+        var self = this;
+        return (
+            <form onSubmit={this.onSubmit}>
+                {Object.keys(this.state.form).map(function (key) {
+                    return(
+                        <TextBox name = {key} value = {self.state.form[key]} onChange = {self.onChange}/>
+                    )
+                })}
+                <button type="submit" className="btn btn-success">submit</button>
+            </form>
+        )
+    }
+})
